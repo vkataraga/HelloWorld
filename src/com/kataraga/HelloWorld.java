50609f9da97882ad8a49e7ff2d43da3a9eb04e15
Viktoriia Kataraga <johndoe@example.com>
2017-05-25 21:48:02 -0700

    my first commit

---
+++ src/com/kataraga/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.kataraga;
+
+/**
+ * Created by В on 25.05.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
