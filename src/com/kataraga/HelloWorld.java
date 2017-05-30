1d42312063656ba8d96a5c5b0a2597117ed1efaa
amarkin <test@test.ru>
2017-05-29 22:21:19 -0700

    feature-am-1b

--- src/com/kataraga/HelloWorld.java
+++ src/com/kataraga/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("1");
+        System.out.println("2");
     }
 }
